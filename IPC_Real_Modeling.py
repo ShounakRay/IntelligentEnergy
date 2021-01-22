b8a9143277304686cfcd9f09cba4046c89bc0b42
ShounakRay <37996385+ShounakRay@users.noreply.github.com>
2021-01-22 14:32:09 -0700

    Modularization, Pre-base Filtering

--- IPC_Real_Modeling.py
+++ IPC_Real_Modeling.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: IPC_Real_Modeling.py
 # @Last modified by:   Ray
-# @Last modified time: 22-Jan-2021 01:01:91:918  GMT-0700
+# @Last modified time: 22-Jan-2021 14:01:27:270  GMT-0700
 # @License: No License for Distribution
 
 # G0TO: CTRL + OPTION + G
@@ -14,6 +14,7 @@
 # PASTE IMAGE: CTRL + OPTION + SHIFT + V
 # Todo: Opt + K  Opt + T
 
+import math
 import os
 
 import numpy as np
@@ -115,6 +116,73 @@ def condense_fiber(well_df, BINS):
     return filtered_test_case
 
 
+def pressure_lambda(row):
+    """Picks the available pressure out of casing and tubing pressures.
+
+    Parameters
+    ----------
+    row : Series
+        The row in the respective DataFrame.
+
+    Returns
+    -------
+    int or nan (float)
+        The chosen pressure.
+
+    """
+    if not math.isnan(row['Casing_Pressure']):
+        # if Casing_Pressure exists
+        return row['Casing_Pressure']
+    elif not math.isnan(row['Tubing_Pressure']):
+        # if Casing_Pressure doesn't exist but Tubing_Pressure does
+        return row['Tubing_Pressure']
+    else:
+        # If neither Casing_Pressure or Tubing_Pressure exist
+        return math.nan
+
+
+def filter_negatives(df, columns):
+    """Filter out all the rows in the DataFrame whose rows contain negatives.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The intitial, unfiltered DataFrame.
+    columns : pandas Index, or any iterable
+        Which columns to consider.
+
+    Returns
+    -------
+    DataFrame
+        The filtered DataFrame, may contain NaNs (intentionally).
+
+    """
+    for num_col in columns:
+        df = df[~(df[num_col] < 0)].reset_index(drop=True)
+    return df
+
+
+def convert_to_date(df, date_col):
+    """Given a DataFrame and it's time column, assign dtype to datetime.date().
+
+    Parameters
+    ----------
+    df : DataFrameFrame
+        The original DataFrame with [ambiguous] dtypes.
+    date_col : object (string)
+        Name of column with time date.
+
+    Returns
+    -------
+    DataFrame
+        A corrected/re-assigned version of the DataFrame.
+
+    """
+    df[date_col] = pd.to_datetime(df[date_col])
+    df[date_col] = [d.date() for d in df[date_col]]
+    return df
+
+
 # Reformat all individual well data
 well_set = {}
 well_docs = [x[0] for x in os.walk(
@@ -143,22 +211,34 @@ for well in well_docs:
         try:
             exec(var_name + ' = reshape_well_data(' + var_name + ')')
         except Exception as e:
-            print("Error manipulating " + var_name)
+            print("Error manipulating " + var_name + ": " + e)
 
     well_set[well.split('/')[-1]] = well_var_names.copy()
     well_var_names.clear()
 
 # Data Processing - DATA_INJECTION
+# Column Filtering, Pressure Reassignment, DateTime Setting
+# > Delete Rows with Negative Numerical Cells
 DATA_INJECTION = DATA_INJECTION_ORIG.reset_index(drop=True)
 DATA_INJECTION.columns = ['Date', 'Pad', 'Well', 'UWI_Identifier', 'Time_On',
                           'Alloc_Steam', 'Meter_Steam', 'Casing_Pressure',
                           'Tubing_Pressure', 'Reason', 'Comment']
-DATA_INJECTION_KEYS = ['Date', 'Pad', 'Well', 'Time_On',
-                       'Meter_Steam', 'Casing_Pressure', 'Tubing_Pressure']
+DATA_INJECTION_KEYS = ['Date', 'Pad', 'Well', 'Meter_Steam',
+                       'Casing_Pressure', 'Tubing_Pressure']
 DATA_INJECTION = DATA_INJECTION[DATA_INJECTION_KEYS]
-DATA_INJECTION['Date'] = pd.to_datetime(DATA_INJECTION['Date'])
+DATA_INJECTION['Pressure'] = DATA_INJECTION.apply(pressure_lambda, axis=1)
+DATA_INJECTION.drop(['Casing_Pressure', 'Tubing_Pressure'],
+                    axis=1, inplace=True)
+# DATA_INJECTION = DATA_INJECTION[~(DATA_INJECTION['Pressure'] < 0)
+#                                 ].reset_index(drop=True)
+DATA_INJECTION = filter_negatives(DATA_INJECTION,
+                                  DATA_INJECTION.select_dtypes(
+                                      include=['float64']).columns)
+DATA_INJECTION = convert_to_date(DATA_INJECTION, 'Date')
+DATA_INJECTION = DATA_INJECTION.infer_objects()
 
 # Data Processing - DATA_PRODUCTION
+# Column Filtering, DateTime Setting, Delete Rows with Negative Numerical Cells
 DATA_PRODUCTION = DATA_PRODUCTION_ORIG.reset_index(drop=True)
 DATA_PRODUCTION.columns = ['Date', 'Pad', 'Well', 'UWI_Identifier', 'Time_On',
                            'Downtime_Code', 'Alloc_Oil', 'Alloc_Water',
@@ -170,12 +250,17 @@ DATA_PRODUCTION.columns = ['Date', 'Pad', 'Well', 'UWI_Identifier', 'Time_On',
 DATA_PRODUCTION_KEYS = ['Date', 'Pad', 'Well', 'Time_On', 'Hourly_Meter_Steam',
                         'Daily_Meter_Steam', 'Pump_Speed',
                         'Tubing_Pressure', 'Casing_Pressure', 'Heel_Pressure',
-                        'Toe_Pressure', 'Heel_Temp', 'Toe_Temp',
-                        'Last_Test_Date']
+                        'Toe_Pressure', 'Heel_Temp', 'Toe_Temp']
 DATA_PRODUCTION = DATA_PRODUCTION[DATA_PRODUCTION_KEYS]
-DATA_PRODUCTION['Date'] = pd.to_datetime(DATA_PRODUCTION['Date'])
+DATA_PRODUCTION = filter_negatives(DATA_PRODUCTION,
+                                   DATA_PRODUCTION.select_dtypes(
+                                       include=['float64']).columns[1:])
+DATA_PRODUCTION = convert_to_date(DATA_PRODUCTION, 'Date')
+DATA_PRODUCTION = DATA_PRODUCTION.infer_objects()
 
 # Data Processing - DATA_TEST
+# Column Filtering, DateTime Setting, Delete Rows with Negative Numerical Cells
+# TODO: Do we care about BSW, Chlorides, Pump_Speed, Pump_Efficiency, Pump_Size
 DATA_TEST = DATA_TEST_ORIG.reset_index(drop=True)
 DATA_TEST.columns = ['Pad', 'Well', 'Start_Time', 'End_Time', 'Duration',
                      'Effective_Date', '24_Fluid', '24_Oil', '24_Hour', 'Oil',
@@ -186,13 +271,19 @@ DATA_TEST.columns = ['Pad', 'Well', 'Start_Time', 'End_Time', 'Duration',
                      'Engineering_Rejected', 'Engineering_Comment']
 DATA_TEST_KEYS = ['Pad', 'Well', 'Duration', 'Effective_Date',
                   '24_Fluid', '24_Oil', '24_Hour',
-                  'Oil', 'Water', 'Gas', 'Fluid', 'BSW', 'Chlorides',
-                  'Pump_Speed', 'Pump_Efficiency', 'Pump_Size']
+                  'Oil', 'Water', 'Gas', 'Fluid']
 DATA_TEST = DATA_TEST[DATA_TEST_KEYS]
-DATA_TEST['Effective_Date'] = pd.to_datetime(DATA_TEST['Effective_Date'])
-DATA_TEST['Effective_Date'] = [d.date() for d in DATA_TEST['Effective_Date']]
-DATA_TEST['Effective_Date'] = pd.to_datetime(DATA_TEST['Effective_Date'])
+DATA_TEST = filter_negatives(DATA_TEST,
+                             DATA_TEST.select_dtypes(
+                                 include=['float64']).columns)
+DATA_PRODUCTION = convert_to_date(DATA_PRODUCTION, 'Effective_Date')
 DATA_TEST.rename(columns={'Effective_Date': 'Date'}, inplace=True)
+DATA_TEST = DATA_TEST.infer_objects()
+
+
+# r,c = np.where(DATA_PRODUCTION.select_dtypes(include=['float64']).values < 0)
+# pd.DataFrame(DATA_PRODUCTION.select_dtypes(include=['float64']).values[r])
+
 
 # TODO: Bin Fiber Data
 test_case = BP16DTS
