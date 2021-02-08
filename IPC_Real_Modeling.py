ec563ce0864c0fb006b6b042beb72507665cb12d
Shounak <rijshouray@gmail.com>
2021-02-08 14:08:48 -0700

    Formatting, Final Commit Before Merge

--- IPC_Real_Modeling.py
+++ IPC_Real_Modeling.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: IPC_Real_Modeling.py
 # @Last modified by:   Ray
-# @Last modified time: 26-Jan-2021 18:01:91:911  GMT-0700
+# @Last modified time: 08-Feb-2021 14:02:02:026  GMT-0700
 # @License: No License for Distribution
 
 # G0TO: CTRL + OPTION + G
