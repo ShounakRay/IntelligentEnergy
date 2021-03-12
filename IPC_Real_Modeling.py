c92fc34a89ec3a40069f241da58ab1aca68eb180
Shounak Ray <rijshouray@gmail.com>
2021-03-12 14:51:09 -0700

    Added production well iteration layer

--- IPC_Real_Modeling.py
+++ IPC_Real_Modeling.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: IPC_Real_Modeling.py
 # @Last modified by:   Ray
-# @Last modified time: 12-Mar-2021 11:03:14:142  GMT-0700
+# @Last modified time: 12-Mar-2021 14:03:28:283  GMT-0700
 # @License: [Private IP]
 
 # HELPFUL NOTES:
@@ -208,7 +208,7 @@ def correlation_matrix(df, FPATH, abs_arg=True, mask=True, annot=False, type_cor
     mask : bool
         Whether only the bottom left triange of the matrix should be rendered.
     annot : bool
-        Whether each cell should be annotated with its numerical value
+        Whether each cell should be annotated with its numerical value.
     type_corrs : list
         All the different correlations that should be provided. Default to all built-in pandas options for df.corr().
     cmap : child of matplotlib.colors
@@ -226,15 +226,16 @@ def correlation_matrix(df, FPATH, abs_arg=True, mask=True, annot=False, type_cor
         Furthermore, heatmap is saved in specifid format and printed to console.
 
     """
+    input_data = {}
 
     # NOTE: Conditional assignment of sns.heatmap based on parameters
     # > Mask sns.heatmap parameter is conditionally controlled
     # > If absolute value is not chosen by the user, switch to divergent heatmap. Otherwise keep it as sequential.
     fig, ax = plt.subplots(ncols=len(type_corrs), sharey=True, figsize=figsize)
     for typec in type_corrs:
-        input_data = (df.corr(typec.lower()).abs()**contrast_factor if abs_arg
-                      else df.corr(typec.lower())**contrast_factor)
-        sns_fig = sns.heatmap(input_data,
+        input_data[typec] = (df.corr(typec.lower()).abs()**contrast_factor if abs_arg
+                             else df.corr(typec.lower())**contrast_factor)
+        sns_fig = sns.heatmap(input_data[typec],
                               mask=np.triu(df.corr().abs()) if mask else None,
                               ax=ax[type_corrs.index(typec)],
                               annot=annot,
@@ -242,6 +243,8 @@ def correlation_matrix(df, FPATH, abs_arg=True, mask=True, annot=False, type_cor
                               ).set_title("{cortype}'s Correlation Matrix".format(cortype=typec))
     plt.tight_layout()
     sns_fig.get_figure().savefig(FPATH, box_inches="tight")
+
+    return input_data
 
 
 print('STATUS: Hyperparameters assigned and functions defined')
@@ -297,36 +300,41 @@ aml_fe_obj = H2OAutoML(max_runtime_secs=MAX_EXP_RUNTIME,          # How long sho
 SENSOR_PREDICTORS = ['Tubing_Pressure', 'Casing_Pressure', 'Heel_Pressure', 'Toe_Pressure', 'Heel_Temp', 'Toe_Temp']
 PRODUCTION_TARGET = 'Oil'
 
-# Only look at data from test days (to minimize/eliminate missing values for PRODUCTION_TARGET)
-TEST_DATA = data[data['test_flag'].isin(['True'])]
-aml_fe_obj.train(x=SENSOR_PREDICTORS,
-                 y=PRODUCTION_TARGET,                                   # A single responder
-                 training_frame=TEST_DATA)                              # All the data is used for training, CV
+# FOR EACH PRODUCTION WELL
+proxy_correlations = {}
+proxy_sensor_rankings = {}
+for prod_well in PRODUCTION_WELLS:
+    # Only look at data from test days (to minimize/eliminate missing values for PRODUCTION_TARGET)
+    TEST_DATA = data[data['test_flag'].isin(['True'])]
+    TEST_DATA = TEST_DATA[TEST_DATA['Well'] == prod_well]
+    aml_fe_obj.train(x=SENSOR_PREDICTORS,
+                     y=PRODUCTION_TARGET,                                   # A single responder
+                     training_frame=TEST_DATA)                              # All the data is used for training, CV
 
-print('STATUS: Selective sensor-predictor model trained.')
+    print('STATUS: Selective sensor-predictor model trained.')
 
-# Determine Variable Importance of Sensor Features
-cumulative_fe_varimps, excluded = exp_cumulative_varimps(aml_fe_obj)
-# Reindex output to name of model (for seaborn labeling)
-cumulative_fe_varimps.index = cumulative_fe_varimps['model_name']
-# Determine the feature rank depending on the mean of each predictor column
-SENSOR_RANKING = cumulative_fe_varimps.select_dtypes(float).mean(axis=0).sort_values(ascending=False)
-# Reorder columns by SENSOR_RANKING, Reorder rows by mean of model's variable importances
-cumulative_fe_varimps = cumulative_fe_varimps.select_dtypes(float)[SENSOR_RANKING.keys()]
-cumulative_fe_varimps = cumulative_fe_varimps.reindex(cumulative_fe_varimps.mean(axis=1).sort_values(axis=0).index,
-                                                      axis=0)
+    # Determine Variable Importance of Sensor Features
+    cumulative_fe_varimps, excluded = exp_cumulative_varimps(aml_fe_obj)
+    # Reindex output to name of model (for seaborn labeling)
+    cumulative_fe_varimps.index = cumulative_fe_varimps['model_name']
+    # Determine the feature rank depending on the mean of each predictor column
+    SENSOR_RANKING = cumulative_fe_varimps.select_dtypes(float).mean(axis=0).sort_values(ascending=False)
+    # Reorder columns by SENSOR_RANKING, Reorder rows by mean of model's variable importances
+    cumulative_fe_varimps = cumulative_fe_varimps.select_dtypes(float)[SENSOR_RANKING.keys()]
+    cumulative_fe_varimps = cumulative_fe_varimps.reindex(cumulative_fe_varimps.mean(axis=1).sort_values(axis=0).index,
+                                                          axis=0)
 
-# NOTE: For reference
-# _ = plt.hist(cumulative_fe_varimps, bins=45, stacked=True)
-# Save oil-sensor correlations to PDF
-fig, ax = plt.subplots(figsize=(8, 16))
-sns_fig = sns.heatmap(cumulative_fe_varimps, annot=True).set_title('Feature Ranking:\nBest -> Worst')
-sns_fig.get_figure().savefig('Modeling Reference Files/cumulative_fe_varimps.pdf', bbox_inches="tight")
+    # Save oil-sensor correlations to PDF
+    fig, ax = plt.subplots(figsize=(8, 16))
+    sns_fig = sns.heatmap(cumulative_fe_varimps, annot=True).set_title('Feature Ranking:\nBest -> Worst')
+    sns_fig.get_figure().savefig('Modeling Reference Files/cumulative_fe_varimps__{PWELL}.pdf'.format(PWELL=prod_well),
+                                 bbox_inches="tight")
 
-# NOTE: For reference
-# Correlation heatmaps
-correlation_matrix(cumulative_fe_varimps,
-                   FPATH='Modeling Reference Files/cumulative_fe_varimps.corr().abs().pdf')
+    # Correlation heatmaps
+    proxy_correlations[prod_well] = correlation_matrix(cumulative_fe_varimps,
+                                                       FPATH='Modeling Reference Files/cumulative_fe_varimps.corr()' +
+                                                       '.abs()__{PWELL}.pdf'.format(PWELL=prod_well))
+    proxy_sensor_rankings[prod_well] = SENSOR_RANKING
 
 print('STATUS: Optimal sensor oil proxies determined. These will now be the responders for future predictive models.')
 
@@ -335,34 +343,34 @@ _ = """
 #######################################   ENGINEERING | MEAN TARGET ENCODING   ########################################
 #######################################################################################################################
 """
-
-# TODO: Mean Target Encoding isn't currently functional due to manual target spec. -> should be dynamically encoded
-# All the target features
-RESPONDERS = data.columns[data.columns.index(FIRST_WELL_STM):]
-
-# The columns should be encoded. Must be categorical, automatically select the categorical ones.
-temp_df = data.as_data_frame().select_dtypes([float, int]).columns
-encoded_columns = [col for col in data.columns if col not in temp_df]
-# Assign kfold assignment randomly (based on constant RANDOM_SEED for reproducibility)
-data[FOLD_COLUMN] = data.kfold_column(n_folds=5, seed=RANDOM_SEED)
-
-# Congifure auto-encoder
-# http://docs.h2o.ai/h2o/latest-stable/h2o-py/docs/_modules/h2o/estimators/targetencoder.html
-data_est_te = H2OTargetEncoderEstimator(data_leakage_handling="k_fold",  # enc. for a fold gen. on out-of-fold data
-                                        fold_column=FOLD_COLUMN,         # The column name with specific fold labels
-                                        blending=True,          # Helps in encoding low-cardinality categoricals
-                                        inflection_point=3,     # Inflection point of sigmoid used to blend pr.
-                                        smoothing=20,           # m^-1 @ inflection point on sigmoid used to blend pr.
-                                        noise=0.01,             # Low data, needs more regularization; set to detault
-                                        seed=RANDOM_SEED)
-# Train the encoder model (this isn't predictive, its manipulative)
-data_est_te.train(x=encoded_columns,
-                  y=RESPONDERS[0],
-                  training_frame=data)
-# Encode the data based on the trained encoder
-data = data_est_te.transform(frame=data, as_training=True)
-
-print('STATUS: Completed Mean Target Encoding for all categorical features.')
+#
+# # TODO: Mean Target Encoding isn't currently functional due to manual target spec. -> should be dynamically encoded
+# # All the target features
+# RESPONDERS = data.columns[data.columns.index(FIRST_WELL_STM):]
+#
+# # The columns should be encoded. Must be categorical, automatically select the categorical ones.
+# temp_df = data.as_data_frame().select_dtypes([float, int]).columns
+# encoded_columns = [col for col in data.columns if col not in temp_df]
+# # Assign kfold assignment randomly (based on constant RANDOM_SEED for reproducibility)
+# data[FOLD_COLUMN] = data.kfold_column(n_folds=5, seed=RANDOM_SEED)
+#
+# # Congifure auto-encoder
+# # http://docs.h2o.ai/h2o/latest-stable/h2o-py/docs/_modules/h2o/estimators/targetencoder.html
+# data_est_te = H2OTargetEncoderEstimator(data_leakage_handling="k_fold",  # enc. for a fold gen. on out-of-fold data
+#                                         fold_column=FOLD_COLUMN,         # The column name with specific fold labels
+#                                         blending=True,          # Helps in encoding low-cardinality categoricals
+#                                         inflection_point=3,     # Inflection point of sigmoid used to blend pr.
+#                                         smoothing=20,           # m^-1 @ inflection point on sigmoid used to blend pr
+#                                         noise=0.01,             # Low data, needs more regularization; set to detault
+#                                         seed=RANDOM_SEED)
+# # Train the encoder model (this isn't predictive, its manipulative)
+# data_est_te.train(x=encoded_columns,
+#                   y=RESPONDERS[0],
+#                   training_frame=data)
+# # Encode the data based on the trained encoder
+# data = data_est_te.transform(frame=data, as_training=True)
+#
+# print('STATUS: Completed Mean Target Encoding for all categorical features.')
 
 _ = """
 #######################################################################################################################
@@ -397,20 +405,25 @@ model_novarimps = []
 for responder in RESPONDERS:
     print('\n{block}{block}STATUS: Responder {RESP}'.format(block=OUT_BLOCK,
                                                             RESP=responder))
-    # Run the experiment (for the specified responder)
-    # NOTE: Fold column specified for cross validation to mitigate leakage (absent if no encoding was performed)
-    # https://docs.h2o.ai/h2o/latest-stable/h2o-py/docs/_modules/h2o/automl/autoh2o.html
-    aml_obj.train(x=PREDICTORS,                                 # All the depedent variables in each model
-                  y=responder,                                  # A single responder
-                  # fold_column=FOLD_COLUMN,                    # Fold column name, as specified from encoding
-                  training_frame=data)                          # All the data is used for training, cross-validation
+    cumulative_varimps[responder] = {}
+    for prod_well in PRODUCTION_WELLS:
+        TEST_DATA = data[data['Well'] == prod_well]
+        # Run the experiment (for the specified responder)
+        # NOTE: Fold column specified for cross validation to mitigate leakage (absent if no encoding was performed)
+        # https://docs.h2o.ai/h2o/latest-stable/h2o-py/docs/_modules/h2o/automl/autoh2o.html
+        aml_obj.train(x=PREDICTORS,                               # All the depedent variables in each model
+                      y=responder,                                # A single responder
+                      # fold_column=FOLD_COLUMN,                  # Fold column name, as specified from encoding
+                      training_frame=TEST_DATA)                   # All the data is used for training, cross-validation
 
-    # Calculate variable importance for these specific responders
-    varimps = exp_cumulative_varimps(aml_obj, tag=[responder], tag_name=['responder'])
-    cumulative_varimps[responder] = varimps
-
+        # Calculate variable importance for these specific responders
+        varimps = exp_cumulative_varimps(aml_obj,
+                                         tag=[prod_well, responder],
+                                         tag_name=['production_well', 'responder'])
+        cumulative_varimps[responder][prod_well] = varimps
 # Concatenate all the individual model variable importances into one dataframe
-final_cumulative_varimps = pd.concat(cumulative_varimps.values()).reset_index(drop=True)
+final_cumulative_varimps = pd.concat([list(pair.values())[0] for pair in list(cumulative_varimps.values())
+                                      ]).reset_index(drop=True)
 # Exclude any features encoded by default (H2O puts a `.` in the column name of these features)
 final_cumulative_varimps = final_cumulative_varimps.loc[:, ~final_cumulative_varimps.columns.str.contains('.',
                                                                                                           regex=False)]
