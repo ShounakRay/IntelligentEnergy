05292e2fef0020d04da7104c413b412c4c3590e6
Shounak Ray <rijshouray@gmail.com>
2021-03-22 13:43:29 -0600

    Geographical Layout into Account

--- approach_alternative.py
+++ approach_alternative.py
@@ -3,12 +3,13 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: approach_alternative.py
 # @Last modified by:   Ray
-# @Last modified time: 19-Mar-2021 16:03:34:348  GMT-0600
+# @Last modified time: 22-Mar-2021 13:03:34:341  GMT-0600
 # @License: [Private IP]
 
 import math
-import sys
+from itertools import chain
 
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -81,6 +82,27 @@ def pressure_lambda(row):
         return math.nan
 
 
+def drop_singles(df):
+    dropped = []
+    for col in df.columns:
+        if len(df[col].unique()) <= 1:
+            dropped.append(col)
+            df.drop(col, axis=1, inplace=True)
+    return df, dropped
+
+
+def intermediates(p1, p2, nb_points=8):
+    """"Return a list of nb_points equally spaced points
+    between p1 and p2"""
+    # If we have 8 intermediate points, we have 8+1=9 spaces
+    # between p1 and p2
+    x_spacing = (p2[0] - p1[0]) / (nb_points + 1)
+    y_spacing = (p2[1] - p1[1]) / (nb_points + 1)
+
+    return [[p1[0] + i * x_spacing, p1[1] + i * y_spacing]
+            for i in range(1, nb_points + 1)]
+
+
 # INGESTION
 FINALE = pd.read_csv('Data/combined_ipc.csv').infer_objects()
 DATA_INJECTION_ORIG = pd.read_pickle('Data/Pickles/DATA_INJECTION_ORIG.pkl')
@@ -89,49 +111,69 @@ DATA_PRODUCTION_ORIG = pd.read_pickle('Data/Pickles/DATA_PRODUCTION_ORIG.pkl')
 PRO_PAD_KEYS = dict(zip(DATA_PRODUCTION_ORIG['Well'], DATA_PRODUCTION_ORIG['Pad']))
 INJ_PAD_KEYS = dict(zip(DATA_INJECTION_ORIG['Well'], DATA_INJECTION_ORIG['Pad']))
 
-# list(FINALE_pro.columns)
+# list(FINALE.columns)
 
-# FINALE PROCESSING
-finale_replace = {'date': 'Date',
-                  'producer_well': 'Well',
-                  'prod_casing_pressure': 'Casing_pressure',
-                  'pad': 'Pad',
-                  'prod_bhp_heel': 'Heel_Pressure',
-                  'prod_bhp_toe': 'Toe_Pressure',
-                  'hours_on_prod': 'Time_On'}
-_ = [finale_replace.update({'bin_{}'.format(i): 'Bin_{}'.format(i)}) for i in range(1, 6)]
-_ = [finale_replace.update({col: col}) for col in FINALE.columns if col not in finale_replace.keys()]
+# [OPTIONAL] FINALE PRE-PROCESSING
+# finale_replace = {'date': 'Date',
+#                   'producer_well': 'Well',
+#                   'prod_casing_pressure': 'Casing_pressure',
+#                   'pad': 'Pad',
+#                   'prod_bhp_heel': 'Heel_Pressure',
+#                   'prod_bhp_toe': 'Toe_Pressure',
+#                   'hours_on_prod': 'Time_On'}
+# _ = [finale_replace.update({'bin_{}'.format(i): 'Bin_{}'.format(i)}) for i in range(1, 6)]
+# _ = [finale_replace.update({col: col}) for col in FINALE.columns if col not in finale_replace.keys()]
+# FINALE.drop(['op_approved', 'eng_approved', 'uwi'], 1, inplace=True)
+# FINALE.columns = FINALE.columns.to_series().map(finale_replace)
 
-FINALE.drop(['op_approved', 'eng_approved', 'uwi'], 1, inplace=True)
-FINALE.columns = FINALE.columns.to_series().map(finale_replace)
-FINALE['Pad'] = FINALE['Well'].apply(lambda x: PRO_PAD_KEYS.get(x))
-FINALE = FINALE.dropna(subset=['Well']).reset_index(drop=True)
+FINALE['PRO_Pad'] = FINALE['PRO_Well'].apply(lambda x: PRO_PAD_KEYS.get(x))
+FINALE = FINALE.dropna(subset=['PRO_Well']).reset_index(drop=True)
 
 # # # # # # # # # # # # # # # # # # # # PAD-LEVEL SENSOR DATA # # # # # # # # # # # # # # # # # # # # # # #
 
-unique_pro_pads = list(FINALE['Pad'].unique())
-all_pro_data = [*FINALE.columns[1:7], *FINALE.columns[-6:]]
+unique_pro_pads = list(FINALE['PRO_Pad'].unique())
+all_pro_data = ['PRO_Well',
+                'PRO_Pump_Speed',
+                'PRO_Time_On',
+                'PRO_Casing_Pressure',
+                'PRO_Heel_Pressure',
+                'PRO_Toe_Pressure',
+                'PRO_Heel_Temp',
+                'PRO_Toe_Temp',
+                'PRO_Pad',
+                'PRO_Duration',
+                'PRO_Oil',
+                'PRO_Water',
+                'PRO_Gas',
+                'PRO_Fluid',
+                'PRO_Chlorides']
 FINALE_pro = FINALE[all_pro_data + ['Date']]
 
-# FINALE_agg = FINALE_pro.groupby(by=['Date', 'Pad'], axis=0, sort=False, as_index=False).sum()
-FINALE_melted_pro = pd.melt(FINALE, id_vars=['Date'], value_vars=all_pro_data, var_name='metric', value_name='Well')
-FINALE_melted_pro['Pad'] = FINALE_melted_pro['Well'].apply(lambda x: PRO_PAD_KEYS.get(x))
+FINALE_pro, dropped_cols = drop_singles(FINALE_pro)
 
-FINALE_agg_pro = FINALE_pro.groupby(by=['Date', 'Pad'], axis=0,
-                                    sort=False, as_index=False).agg({'spm': 'sum',
-                                                                     'Time_On': 'mean',
-                                                                     'Casing_pressure': 'mean',
-                                                                     'Heel_Pressure': 'mean',
-                                                                     'Toe_Pressure': 'mean',
-                                                                     'test_oil': 'sum',
-                                                                     'test_water': 'sum',
-                                                                     'pump_size': 'mean',
-                                                                     'pump_efficiency': 'mean'})
+# FINALE_agg = FINALE_pro.groupby(by=['Date', 'Pad'], axis=0, sort=False, as_index=False).sum()
+FINALE_melted_pro = pd.melt(FINALE, id_vars=['Date'], value_vars=all_pro_data, var_name='metric', value_name='PRO_Pad')
+FINALE_melted_pro['PRO_Pad'] = FINALE_melted_pro['PRO_Pad'].apply(lambda x: PRO_PAD_KEYS.get(x))
+
+FINALE_agg_pro = FINALE_pro.groupby(by=['Date', 'PRO_Pad'], axis=0,
+                                    sort=False, as_index=False).agg({'PRO_Pump_Speed': 'sum',
+                                                                     'PRO_Time_On': 'mean',
+                                                                     'PRO_Casing_Pressure': 'mean',
+                                                                     'PRO_Heel_Pressure': 'mean',
+                                                                     'PRO_Duration': 'mean',
+                                                                     'PRO_Toe_Pressure': 'mean',
+                                                                     'PRO_Oil': 'sum',
+                                                                     'PRO_Water': 'sum',
+                                                                     'PRO_Gas': 'sum',
+                                                                     'PRO_Fluid': 'sum',
+                                                                     'PRO_Chlorides': 'sum'})
+
+# FIGURE PLOTTING (PRODUCTION PAD-LEVEL STATISTICS)
 master_rows = len(unique_pro_pads)
 master_cols = len(FINALE_agg_pro.select_dtypes(float).columns)
 fig, ax = plt.subplots(nrows=master_rows, ncols=master_cols, figsize=(200, 50))
 for pad in unique_pro_pads:
-    temp_pad = FINALE_agg_pro[FINALE_agg_pro['Pad'] == pad].sort_values('Date').reset_index(drop=True)
+    temp_pad = FINALE_agg_pro[FINALE_agg_pro['PRO_Pad'] == pad].sort_values('Date').reset_index(drop=True)
     d_1 = list(temp_pad['Date'])[0]
     d_n = list(temp_pad['Date'])[-1]
     numcols = FINALE_agg_pro.select_dtypes(float).columns
@@ -152,18 +194,20 @@ plt.savefig('pro_pads_cols_ts.png')
 
 # # # # # # # # # # # # # # # # INJECTOR PAD-LEVEL STEAM ALLOCATION # # # # # # # # # # # # # # # # # # # #
 
-FINALE_inj = FINALE[FINALE['Well'] == 'AP3'].reset_index(drop=True).drop('Well', 1)
-all_injs = [c for c in FINALE_inj.columns if 'I' in c]
+FINALE_inj = FINALE[FINALE['PRO_Well'] == 'AP3'].reset_index(drop=True).drop('PRO_Well', 1)
+all_injs = [c for c in FINALE_inj.columns if 'I' in c and '_' not in c]
 FINALE_melted_inj = pd.melt(FINALE_inj, id_vars=['Date'], value_vars=all_injs,
                             var_name='Injector', value_name='Steam')
-FINALE_melted_inj['Pad'] = FINALE_melted_inj['Injector'].apply(lambda x: INJ_PAD_KEYS.get(x))
-FINALE_agg_inj = FINALE_melted_inj.groupby(by=['Date', 'Pad'], axis=0, sort=False, as_index=False).sum()
+FINALE_melted_inj['INJ_Pad'] = FINALE_melted_inj['Injector'].apply(lambda x: INJ_PAD_KEYS.get(x))
+FINALE_melted_inj = FINALE_melted_inj[~FINALE_melted_inj['INJ_Pad'].isna()].reset_index(drop=True)
+FINALE_agg_inj = FINALE_melted_inj.groupby(by=['Date', 'INJ_Pad'], axis=0, sort=False, as_index=False).sum()
 
-unique_inj_pads = list(FINALE_agg_inj['Pad'].unique())
+# FIGURE PLOTTING (INJECTION PAD-LEVEL STATISTICS)
+unique_inj_pads = list(FINALE_agg_inj['INJ_Pad'].unique())
 fig, ax = plt.subplots(nrows=len(unique_inj_pads), figsize=(15, 80))
 for pad in unique_inj_pads:
     subp = ax[unique_inj_pads.index(pad)]
-    temp = FINALE_agg_inj[FINALE_agg_inj['Pad'] == pad].sort_values('Date').reset_index(drop=True)
+    temp = FINALE_agg_inj[FINALE_agg_inj['INJ_Pad'] == pad].sort_values('Date').reset_index(drop=True)
     d_1 = list(temp['Date'])[0]
     d_n = list(temp['Date'])[-1]
     subp.plot(temp['Steam'], label='Injector ' + pad + '\n{} > {}'.format(d_1, d_n))
@@ -175,4 +219,85 @@ plt.savefig('inj_pads_ts.png')
 
 # # # # # # # # # # # # # # # # INJECTOR ASSOCIATIONS # # # # # # # # # # # # # # # # # # # #
 
-dict(zip(FINALE_melted_inj['Injector'], FINALE_melted_inj['Pad']))
+# Injector wells and injector pad relationships
+INJ_well_to_pad = dict(zip(FINALE_melted_inj['Injector'], FINALE_melted_inj['INJ_Pad']))
+# Injector wells to producer well overlaps (only those spanning different producer pads)
+INJ_well_to_pad_overlaps = {'I16': ['AP3', 'BP1'],
+                            'I21': ['AP3', 'BP1'],
+                            'I27': ['AP3', 'BP1'],
+                            'I37': ['FP1', 'BP6'],
+                            'I72': ['FP1', 'BP6'],
+                            'I47': ['CP2', 'EP2'],
+                            'I44': ['CP3', 'EP2'],
+                            'I42': ['CP6', 'EP2'],
+                            'CI7': ['CP7', 'EP2'],
+                            'CI8': ['CP8', 'EP2']}
+PRO_injpad_to_well = {'E3': ['EP2', 'EP3', 'EP4', 'EP5', 'EP6', 'EP7'],
+                      'E2': ['EP2', 'EP3', 'EP4', 'EP5', 'EP6', 'EP7'],
+                      'E1': ['EP2', 'EP3', 'EP4', 'EP5', 'EP6', 'EP7'],
+                      'A': ['AP4', 'AP5', 'AP6', 'AP7', 'A8'],
+                      '15-05': ['AP4', 'AP6', 'AP7'],
+                      '16-05': ['AP2', 'AP3', 'AP4', 'AP5', 'AP6', 'AP7']}
+
+PRO_relcoords = {'AP2': '(696, 554) <> (995, 551)',
+                 'AP3': '(700, 582) <> (995, 582)',
+                 'AP4': '(691, 526) <> (1052, 523)',
+                 'AP5': '(684, 498) <> (759, 507) <> (1058, 501)',
+                 'AP6': '(695, 478) <> (827, 472) <> (910, 472)',
+                 'AP7': '(693, 454) <> (846, 452) <> (992, 450)',
+                 'AP8': '(690, 429) <> (910, 427)',
+                 'BP1': '(684, 633) <> (992, 635) <> (1015, 620)',
+                 'BP2': '(708, 668) <> (1014, 668)',
+                 'BP3': '(703, 702) <> (1016, 697)',
+                 'BP4': '(663, 752) <> (908, 750)',
+                 'BP5': '(676, 783) <> (838, 786) <> (1010, 745)',
+                 'BP6': '(690, 821) <> (1026, 817)'}
+POS_TL = (478, 71)
+POS_TR = (1439, 71)
+POS_BL = (478, 1008)
+POS_BR = (1439, 1008)
+PRO_relcoords = {}
+# Get relative position inputs
+for well in [pw for pw in PRO_PAD_KEYS.keys() if 'A' in pw or 'B' in pw]:
+    PRO_relcoords[well] = input(prompt='Please enter coordinates for {}'.format(well))
+# Re-format relative positions
+for k, v in PRO_relcoords.items():
+    PRO_relcoords[k] = [chunk.strip() for chunk in v.split('<>')]
+    PRO_relcoords[k] = [eval(chunk) for chunk in PRO_relcoords[k]]
+# Re-scale relative positions (cartesian, not jS, system)
+x_delta = POS_TL[0] | POS_BL[0]
+y_delta = POS_TR[0] | POS_BR[0]
+for k, v in PRO_relcoords.items():
+    transformed = []
+    for coordinate in PRO_relcoords[k]:
+        transformed.append((coordinate[0] - x_delta, y_delta - coordinate[1]))
+    PRO_relcoords[k] = transformed
+# Find n points connecting the points
+connections = {}
+arbitrary_length = 40
+for k, v in PRO_relcoords.items():
+    discrete_links = []
+    for coordinate_i in range(len(PRO_relcoords[k]) - 1):
+        c1 = PRO_relcoords[k][coordinate_i]
+        c2 = PRO_relcoords[k][coordinate_i + 1]
+        x1 = c1[0]
+        x2 = c2[0]
+        y1 = c1[1]
+        y2 = c2[1]
+        num_points = int(math.hypot(x2 - x1, y2 - y1) / arbitrary_length)
+        discrete_ind = intermediates(c1, c2, nb_points=num_points)
+        discrete_links.append(discrete_ind)
+    connections[k] = list(chain.from_iterable(discrete_links))
+# Plot connections to confirm
+colors = cm.rainbow(np.linspace(0, 1, len(connections.keys())))
+for k, v in connections.items():
+    all_x = [c[0] for c in v]
+    all_y = [c[1] for c in v]
+    plt.scatter(all_x, all_y, linestyle='solid', color=colors[list(connections.keys()).index(k)])
+    plt.plot(all_x, all_y, color=colors[list(connections.keys()).index(k)])
+
+# color=colors[list(PRO_relcoords.keys()).index(k)]
+list(chain.from_iterable(discrete_links))
+# EOF
+
+# EOF
--- base_generation.py
+++ base_generation.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: base_generation.py
 # @Last modified by:   Ray
-# @Last modified time: 22-Mar-2021 10:03:86:863  GMT-0600
+# @Last modified time: 22-Mar-2021 10:03:48:485  GMT-0600
 # @License: [Private IP]
 
 
@@ -12,6 +12,8 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# # # # # # # # # # # # # # # # # # # # # # # #
+# SOURCE INGESTION
 data_dir = 'Data/Isolated/'
 fiber_dir = 'Data/DTS/'
 
@@ -20,7 +22,7 @@ producers = pd.read_excel(data_dir + "OLT production data (rev 1).xlsx")
 well_test = pd.read_excel(data_dir + "OLT well test data.xlsx")
 
 # # # # # # # # # # # # # # # # # # # # # # # #
-# DATA MANIPULATION FUNCTIONS
+# DATA TRANSOFOMATION FUNCTIONS
 
 
 def combine_data(producer):
@@ -51,30 +53,13 @@ def get_fiber_data(producer, bins=5):
     condensed = []
     for i, s in enumerate(range(0, max_length + 1, segment_length)):
         condensed.append(pd.DataFrame(
-            {'bin_' + str(i + 1): list(combined.iloc[:, s:s + segment_length].mean(axis=1))}))
+            {'Bin_' + str(i + 1): list(combined.iloc[:, s:s + segment_length].mean(axis=1))}))
 
     condensed = pd.concat(condensed, axis=1)
-    condensed['date'] = combined['index']
-    condensed = condensed.sort_values('date').set_index('date')
+    condensed['Date'] = combined['index']
+    condensed = condensed.sort_values('Date').set_index('Date')
 
     return condensed
-
-
-# # # # # # # # # # # # # # # # # # # # # # # #
-# WELL TEST FORMATTING
-well_test.columns = ['Pad', 'Well', 'Start_Time', 'End_Time', 'Duration',
-                     'Effective_Date', '24_Fluid', '24_Oil', '24_Water', 'Oil',
-                     'Water', 'Gas', 'Fluid', 'BSW', 'Chlorides',
-                     'Pump_Speed', 'Pump_Efficiency', 'Pump_Size',
-                     'Operator_Approved', 'Operator_Rejected',
-                     'Operator_Comment', 'Engineering_Approved',
-                     'Engineering_Rejected', 'Engineering_Comment']
-well_test = well_test[['Pad', 'Well', 'Start_Time', 'Duration', 'Oil', 'Water', 'Gas', 'Fluid', 'Chlorides']]
-# well_test = well_test[['Start Date/Time (yyyy/mm/dd HH:mm)', 'Pad', 'Well', '24 Hour Oil (m3)', '24 Hour Water (m3)',
-#                        'Oil (m3)', 'Water (m3)', 'Gas (m3)''Pump Speed (SPM)', 'Pump Size in', 'Pump Efficiency (%)',
-#                        'Operator Approved', 'Engineering Approved']]
-well_test['Date'] = pd.to_datetime(well_test['Start_Time'])
-well_test.drop('Start_Time', 1, inplace=True)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # #
@@ -84,20 +69,40 @@ producer_wells = [p for p in os.listdir(fiber_dir) if p[0] != '.']
 aggregated_fiber = []
 for producer in producer_wells[1:]:
     condensed = get_fiber_data(producer)
-    condensed['producer_well'] = producer
+    condensed['PRO_Well'] = producer
     aggregated_fiber.append(condensed)
 
 aggregated_fiber = pd.concat(aggregated_fiber)
 aggregated_fiber = aggregated_fiber.dropna(how='all', axis=1)
 
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+# WELL TEST FORMATTING
+well_test.columns = ['PRO_Pad', 'PRO_Well', 'PRO_Start_Time', 'PRO_End_Time', 'PRO_Duration',
+                     'PRO_Effective_Date', 'PRO_24_Fluid', 'PRO_24_Oil', 'PRO_24_Water', 'PRO_Oil',
+                     'PRO_Water', 'PRO_Gas', 'PRO_Fluid', 'PRO_BSW', 'PRO_Chlorides',
+                     'PRO_Pump_Speed', 'PRO_Pump_Efficiency', 'PRO_Pump_Size',
+                     'PRO_Operator_Approved', 'PRO_Operator_Rejected',
+                     'PRO_Operator_Comment', 'PRO_Engineering_Approved',
+                     'PRO_Engineering_Rejected', 'PRO_Engineering_Comment']
+well_test = well_test[['PRO_Pad', 'PRO_Well', 'PRO_Start_Time', 'PRO_Duration',
+                       'PRO_Oil', 'PRO_Water', 'PRO_Gas', 'PRO_Fluid', 'PRO_Chlorides']]
+# well_test = well_test[['Start Date/Time (yyyy/mm/dd HH:mm)', 'Pad', 'Well', '24 Hour Oil (m3)', '24 Hour Water (m3)',
+#                        'Oil (m3)', 'Water (m3)', 'Gas (m3)''Pump Speed (SPM)', 'Pump Size in', 'Pump Efficiency (%)',
+#                        'Operator Approved', 'Engineering Approved']]
+well_test['Date'] = pd.to_datetime(well_test['PRO_Start_Time'])
+well_test.drop('PRO_Start_Time', 1, inplace=True)
+
+
 # # # # # # # # # # # # # # # # # # # # # # # #
 # INJECTOR FORMATTING
 # injectors.columns = ['date', 'pad', 'injector_well', 'uwi', 'hours_on_inj', 'alloc_steam', 'steam',
 #                      'inj_bhp', 'inj_tubing_pressure', 'Reason', 'Comment']
-injectors.columns = ['Date', 'INJ_Pad', 'INJ_Well', 'INJ_UWI_Identifier', 'INJ_Time_On',
+injectors.columns = ['Date', 'INJ_Pad', 'INJ_Well', 'INJ_UWI', 'INJ_Time_On',
                      'INJ_Alloc_Steam', 'INJ_Meter_Steam', 'INJ_Casing_BHP',
                      'INJ_Tubing_Pressure', 'INJ_Reason', 'INJ_Comment']
 inj_cols = ['Date', 'INJ_Pad', 'INJ_Well', 'INJ_Meter_Steam', 'INJ_Casing_BHP', 'INJ_Tubing_Pressure']
+
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 # PRODUCER FORMATTING
@@ -109,39 +114,50 @@ inj_cols = ['Date', 'INJ_Pad', 'INJ_Well', 'INJ_Meter_Steam', 'INJ_Casing_BHP', 
 #                      'prod_casing_pressure', 'prod_bhp_heel', 'prod_bhp_toe',
 #                      'subcool_heel', 'subcool_toe', 'last_test_date',
 #                      'Reason Code', 'Comment']
-producers.columns = ['Date', 'PRO_Pad', 'PRO_Well', 'PRO_UWI_Identifier', 'PRO_Time_On',
+producers.columns = ['Date', 'PRO_Pad', 'PRO_Well', 'PRO_UWI', 'PRO_Time_On',
                      'PRO_Downtime_Code', 'PRO_Alloc_Oil', 'PRO_Alloc_Water',
                      'PRO_Alloc_Gas', 'PRO_Alloc_Steam', 'PRO_Alloc_Steam_To_Producer',
                      'PRO_Hourly_Meter_Steam', 'PRO_Daily_Meter_Steam',
                      'PRO_Pump_Speed', 'PRO_Tubing_Pressure', 'PRO_Casing_Pressure',
                      'PRO_Heel_Pressure', 'PRO_Toe_Pressure', 'PRO_Heel_Temp',
                      'PRO_Toe_Temp', 'PRO_Last_Test_Date', 'PRO_Reason', 'PRO_Comment']
-prod_cols = ['Date', 'UWI', 'PRO_Well', 'PRO_Pump_Speed', 'PRO_Time_On',
+prod_cols = ['Date', 'PRO_UWI', 'PRO_Well', 'PRO_Pump_Speed', 'PRO_Time_On',
              'PRO_Casing_Pressure', 'PRO_Heel_Pressure', 'PRO_Toe_Pressure', 'PRO_Heel_Temp', 'PRO_Toe_Temp',
              'PRO_Alloc_Oil', 'PRO_Alloc_Water']
 
+
 # # # # # # # # # # # # # # # # # # # # # # # #
 # INJECTOR TRANSFORMATION
-injector_table = pd.pivot_table(injectors, values='steam', index='date', columns='injector_well').reset_index()
-injector_table['date'] = pd.to_datetime(injector_table['date']).astype(str)
+injector_table = pd.pivot_table(injectors, values='INJ_Meter_Steam', index='Date', columns='INJ_Well').reset_index()
+# injector_table['date'] = pd.to_datetime(injector_table['date']).astype(str)
+
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 # PRODUCER TRANSFORMATION
-producers['date'] = pd.to_datetime(producers['date']).astype(str)
+# producers['date'] = pd.to_datetime(producers['date']).astype(str)
+
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 # FIBER TRANSFORMATION
 aggregated_fiber = aggregated_fiber.reset_index()
-aggregated_fiber['date'] = pd.to_datetime(aggregated_fiber['date']).astype(str)
+# aggregated_fiber['date'] = pd.to_datetime(aggregated_fiber['date']).astype(str)
+
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 # [OPTIONAL] DTYPE INFERENCE
-
+producers = producers.infer_objects()
+aggregated_fiber = aggregated_fiber.infer_objects()
+injector_table = injector_table.infer_objects()
+well_test = well_test.infer_objects()
+for df in [producers, aggregated_fiber, injector_table, well_test]:
+    df['Date'] = pd.to_datetime(df['Date'])
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 # SOURCE MERGING
-df = pd.merge(producers[prod_cols], aggregated_fiber, how='outer', on=['date', 'producer_well'])
-df = pd.merge(df, injector_table, how='outer', on=['date'])
-df = pd.merge(df, well_test, how='left', on=['date', 'producer_well'])
+df = pd.merge(producers[prod_cols], aggregated_fiber, how='outer', on=['Date', 'PRO_Well'])
+df = pd.merge(df, injector_table, how='outer', on=['Date'])
+df = pd.merge(df, well_test, how='left', on=['Date', 'PRO_Well'])
+
+df = df.infer_objects()
 
 df.to_csv('Data/combined_ipc.csv', index=False)
