0f66486d65e7d7150a0a427c50aeb8b7d0399a0c
Shounak Ray <rijshouray@gmail.com>
2021-03-22 10:43:33 -0600

    Re-assignment of wanted columns, comments

--- base_generation.py
+++ base_generation.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: base_generation.py
 # @Last modified by:   Ray
-# @Last modified time: 19-Mar-2021 14:03:25:259  GMT-0600
+# @Last modified time: 22-Mar-2021 10:03:86:863  GMT-0600
 # @License: [Private IP]
 
 
@@ -19,15 +19,8 @@ injectors = pd.read_excel(data_dir + "OLT injection data.xlsx")
 producers = pd.read_excel(data_dir + "OLT production data (rev 1).xlsx")
 well_test = pd.read_excel(data_dir + "OLT well test data.xlsx")
 
-# well_test.columns
-well_test = well_test[['Start Date/Time (yyyy/mm/dd HH:mm)', 'Pad', 'Well', '24 Hour Oil (m3)', '24 Hour Water (m3)',
-                       'Pump Speed (SPM)', 'Pump Size in', 'Pump Efficiency (%)', 'Operator Approved',
-                       'Engineering Approved']]
-well_test.columns = ['date', 'pad', 'producer_well', 'test_oil',
-                     'test_water', 'test_spm',
-                     'pump_size', 'pump_efficiency', 'op_approved',
-                     'eng_approved']
-well_test['date'] = well_test['date'].astype(str).str.split(" ").str[0]
+# # # # # # # # # # # # # # # # # # # # # # # #
+# DATA MANIPULATION FUNCTIONS
 
 
 def combine_data(producer):
@@ -67,6 +60,25 @@ def get_fiber_data(producer, bins=5):
     return condensed
 
 
+# # # # # # # # # # # # # # # # # # # # # # # #
+# WELL TEST FORMATTING
+well_test.columns = ['Pad', 'Well', 'Start_Time', 'End_Time', 'Duration',
+                     'Effective_Date', '24_Fluid', '24_Oil', '24_Water', 'Oil',
+                     'Water', 'Gas', 'Fluid', 'BSW', 'Chlorides',
+                     'Pump_Speed', 'Pump_Efficiency', 'Pump_Size',
+                     'Operator_Approved', 'Operator_Rejected',
+                     'Operator_Comment', 'Engineering_Approved',
+                     'Engineering_Rejected', 'Engineering_Comment']
+well_test = well_test[['Pad', 'Well', 'Start_Time', 'Duration', 'Oil', 'Water', 'Gas', 'Fluid', 'Chlorides']]
+# well_test = well_test[['Start Date/Time (yyyy/mm/dd HH:mm)', 'Pad', 'Well', '24 Hour Oil (m3)', '24 Hour Water (m3)',
+#                        'Oil (m3)', 'Water (m3)', 'Gas (m3)''Pump Speed (SPM)', 'Pump Size in', 'Pump Efficiency (%)',
+#                        'Operator Approved', 'Engineering Approved']]
+well_test['Date'] = pd.to_datetime(well_test['Start_Time'])
+well_test.drop('Start_Time', 1, inplace=True)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+# FIBER DATA INGESTION AND FORMATTING
 producer_wells = [p for p in os.listdir(fiber_dir) if p[0] != '.']
 
 aggregated_fiber = []
@@ -78,34 +90,58 @@ for producer in producer_wells[1:]:
 aggregated_fiber = pd.concat(aggregated_fiber)
 aggregated_fiber = aggregated_fiber.dropna(how='all', axis=1)
 
-injectors.columns = ['date', 'pad', 'injector_well', 'uwi', 'hours_on_inj', 'alloc_steam', 'steam',
-                     'inj_bhp', 'inj_tubing_pressure', 'Reason', 'Comment']
-inj_cols = ['date', 'injector_well', 'steam', 'inj_bhp', 'inj_tubing_pressure']
+# # # # # # # # # # # # # # # # # # # # # # # #
+# INJECTOR FORMATTING
+# injectors.columns = ['date', 'pad', 'injector_well', 'uwi', 'hours_on_inj', 'alloc_steam', 'steam',
+#                      'inj_bhp', 'inj_tubing_pressure', 'Reason', 'Comment']
+injectors.columns = ['Date', 'INJ_Pad', 'INJ_Well', 'INJ_UWI_Identifier', 'INJ_Time_On',
+                     'INJ_Alloc_Steam', 'INJ_Meter_Steam', 'INJ_Casing_BHP',
+                     'INJ_Tubing_Pressure', 'INJ_Reason', 'INJ_Comment']
+inj_cols = ['Date', 'INJ_Pad', 'INJ_Well', 'INJ_Meter_Steam', 'INJ_Casing_BHP', 'INJ_Tubing_Pressure']
 
-producers.columns = ['date', 'pad', 'producer_well', 'uwi', 'hours_on_prod',
-                     'Downtime Reason Code', 'oil', 'water',
-                     'gas', 'Allocated Injector Steam (m3)',
-                     'Allocated Total Steam To Producer (m3)', 'Metered Steam (m3/hr)',
-                     'Metered Steam (m3/day)', 'spm', 'prod_tubing_pressure',
-                     'prod_casing_pressure', 'prod_bhp_heel', 'prod_bhp_toe',
-                     'subcool_heel', 'subcool_toe', 'last_test_date',
-                     'Reason Code', 'Comment']
+# # # # # # # # # # # # # # # # # # # # # # # #
+# PRODUCER FORMATTING
+# producers.columns = ['date', 'pad', 'producer_well', 'uwi', 'hours_on_prod',
+#                      'Downtime Reason Code', 'oil', 'water',
+#                      'gas', 'Allocated Injector Steam (m3)',
+#                      'Allocated Total Steam To Producer (m3)', 'Metered Steam (m3/hr)',
+#                      'Metered Steam (m3/day)', 'spm', 'prod_tubing_pressure',
+#                      'prod_casing_pressure', 'prod_bhp_heel', 'prod_bhp_toe',
+#                      'subcool_heel', 'subcool_toe', 'last_test_date',
+#                      'Reason Code', 'Comment']
+producers.columns = ['Date', 'PRO_Pad', 'PRO_Well', 'PRO_UWI_Identifier', 'PRO_Time_On',
+                     'PRO_Downtime_Code', 'PRO_Alloc_Oil', 'PRO_Alloc_Water',
+                     'PRO_Alloc_Gas', 'PRO_Alloc_Steam', 'PRO_Alloc_Steam_To_Producer',
+                     'PRO_Hourly_Meter_Steam', 'PRO_Daily_Meter_Steam',
+                     'PRO_Pump_Speed', 'PRO_Tubing_Pressure', 'PRO_Casing_Pressure',
+                     'PRO_Heel_Pressure', 'PRO_Toe_Pressure', 'PRO_Heel_Temp',
+                     'PRO_Toe_Temp', 'PRO_Last_Test_Date', 'PRO_Reason', 'PRO_Comment']
+prod_cols = ['Date', 'UWI', 'PRO_Well', 'PRO_Pump_Speed', 'PRO_Time_On',
+             'PRO_Casing_Pressure', 'PRO_Heel_Pressure', 'PRO_Toe_Pressure', 'PRO_Heel_Temp', 'PRO_Toe_Temp',
+             'PRO_Alloc_Oil', 'PRO_Alloc_Water']
 
-prod_cols = ['date', 'uwi', 'producer_well', 'spm', 'hours_on_prod',
-             'prod_casing_pressure', 'prod_bhp_heel', 'prod_bhp_toe', 'oil', 'water']
-
-
+# # # # # # # # # # # # # # # # # # # # # # # #
+# INJECTOR TRANSFORMATION
 injector_table = pd.pivot_table(injectors, values='steam', index='date', columns='injector_well').reset_index()
 injector_table['date'] = pd.to_datetime(injector_table['date']).astype(str)
 
+# # # # # # # # # # # # # # # # # # # # # # # #
+# PRODUCER TRANSFORMATION
 producers['date'] = pd.to_datetime(producers['date']).astype(str)
 
+# # # # # # # # # # # # # # # # # # # # # # # #
+# FIBER TRANSFORMATION
 aggregated_fiber = aggregated_fiber.reset_index()
 aggregated_fiber['date'] = pd.to_datetime(aggregated_fiber['date']).astype(str)
 
+# # # # # # # # # # # # # # # # # # # # # # # #
+# [OPTIONAL] DTYPE INFERENCE
+
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+# SOURCE MERGING
 df = pd.merge(producers[prod_cols], aggregated_fiber, how='outer', on=['date', 'producer_well'])
 df = pd.merge(df, injector_table, how='outer', on=['date'])
 df = pd.merge(df, well_test, how='left', on=['date', 'producer_well'])
 
 df.to_csv('Data/combined_ipc.csv', index=False)
-list(df.columns)
