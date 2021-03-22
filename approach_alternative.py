88ed2443ce60631c9e5e5ea78708b14bb09e0396
Shounak Ray <rijshouray@gmail.com>
2021-03-22 14:27:32 -0600

    Complete geographical transformation

--- approach_alternative.py
+++ approach_alternative.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: approach_alternative.py
 # @Last modified by:   Ray
-# @Last modified time: 22-Mar-2021 13:03:34:341  GMT-0600
+# @Last modified time: 22-Mar-2021 14:03:59:597  GMT-0600
 # @License: [Private IP]
 
 import math
@@ -239,6 +239,69 @@ PRO_injpad_to_well = {'E3': ['EP2', 'EP3', 'EP4', 'EP5', 'EP6', 'EP7'],
                       '15-05': ['AP4', 'AP6', 'AP7'],
                       '16-05': ['AP2', 'AP3', 'AP4', 'AP5', 'AP6', 'AP7']}
 
+# Scaling, window constants
+POS_TL = (478, 71)
+POS_TR = (1439, 71)
+POS_BL = (478, 1008)
+POS_BR = (1439, 1008)
+x_delta = POS_TL[0] | POS_BL[0]
+y_delta = POS_TR[0] | POS_BR[0]
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # #
+# INJECTOR COORDINATE TRANSFORMATIONS
+INJ_relcoords = {}
+INJ_relcoords = {'I02': '(757, 534)',
+                 'I03': '(709, 519)',
+                 'I04': '(701, 492)',
+                 'I05': '(708, 492)',
+                 'I06': '(825, 537)',
+                 'I07': '(823, 461)',
+                 'I08': '(997, 571)',
+                 'I09': '(940, 516)',
+                 'I10': '(872, 489)',
+                 'I11': '(981, 477)',
+                 'I12': '(1026, 495)',
+                 'I13': '(1034, 444)',
+                 'I14': '(935, 440)',
+                 'I15': '(709, 686)',
+                 'I16': '(694, 611)',
+                 'I17': '(758, 649)',
+                 'I18': '(760, 571)',
+                 'I19': '(818, 684)',
+                 'I20': '(880, 645)',
+                 'I21': '(817, 606)',
+                 'I22': '(881, 565)',
+                 'I23': '(946, 682)',
+                 'I24': '(1066, 679)',
+                 'I25': '(1063, 604)',
+                 'I26': '(995, 643)',
+                 'I27': '(940, 604)',
+                 'I28': '(758, 801)',
+                 'I29': '(701, 766)',
+                 'I30': '(825, 763)',
+                 'I31': '(759, 736)',
+                 'I32': '(871, 716)',
+                 'I33': '(943, 739)',
+                 'I34': '(873, 801)',
+                 'I35': '(1022, 729)',
+                 'I36': '(993, 739)',
+                 'I37': '(1061, 782)',
+                 'I38': '(982, 529)'}
+for inj in [k for k in INJ_PAD_KEYS.keys() if INJ_PAD_KEYS[k] in ['A', '15-05', '16-05', '11-05', '10-05',
+                                                                  '09-05', '06-05', '08-05']]:
+    INJ_relcoords[inj] = input(prompt='Please enter coordinates for injector {}'.format(inj))
+for k, v in INJ_relcoords.items():
+    # String to tuple
+    INJ_relcoords[k] = eval(v)
+    v = INJ_relcoords[k]
+    # Re-scaling
+    INJ_relcoords[k] = (v[0] - x_delta, y_delta - v[1])
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # #
+# PRODUCER COORDINATE TRANSFORMATIONS
+PRO_relcoords = {}
 PRO_relcoords = {'AP2': '(696, 554) <> (995, 551)',
                  'AP3': '(700, 582) <> (995, 582)',
                  'AP4': '(691, 526) <> (1052, 523)',
@@ -252,21 +315,16 @@ PRO_relcoords = {'AP2': '(696, 554) <> (995, 551)',
                  'BP4': '(663, 752) <> (908, 750)',
                  'BP5': '(676, 783) <> (838, 786) <> (1010, 745)',
                  'BP6': '(690, 821) <> (1026, 817)'}
-POS_TL = (478, 71)
-POS_TR = (1439, 71)
-POS_BL = (478, 1008)
-POS_BR = (1439, 1008)
-PRO_relcoords = {}
 # Get relative position inputs
 for well in [pw for pw in PRO_PAD_KEYS.keys() if 'A' in pw or 'B' in pw]:
-    PRO_relcoords[well] = input(prompt='Please enter coordinates for {}'.format(well))
+    PRO_relcoords[well] = input(prompt='Please enter coordinates for producer {}'.format(well))
 # Re-format relative positions
 for k, v in PRO_relcoords.items():
+    # Parsing
     PRO_relcoords[k] = [chunk.strip() for chunk in v.split('<>')]
+    # String to tuple
     PRO_relcoords[k] = [eval(chunk) for chunk in PRO_relcoords[k]]
 # Re-scale relative positions (cartesian, not jS, system)
-x_delta = POS_TL[0] | POS_BL[0]
-y_delta = POS_TR[0] | POS_BR[0]
 for k, v in PRO_relcoords.items():
     transformed = []
     for coordinate in PRO_relcoords[k]:
@@ -274,7 +332,7 @@ for k, v in PRO_relcoords.items():
     PRO_relcoords[k] = transformed
 # Find n points connecting the points
 connections = {}
-arbitrary_length = 40
+arbitrary_length = 30
 for k, v in PRO_relcoords.items():
     discrete_links = []
     for coordinate_i in range(len(PRO_relcoords[k]) - 1):
@@ -288,13 +346,24 @@ for k, v in PRO_relcoords.items():
         discrete_ind = intermediates(c1, c2, nb_points=num_points)
         discrete_links.append(discrete_ind)
     connections[k] = list(chain.from_iterable(discrete_links))
-# Plot connections to confirm
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # #
+# CONFIRM TRANSFORMATIONS
+# Producer connections
+fig, ax = plt.subplots(figsize=(40, 20))
 colors = cm.rainbow(np.linspace(0, 1, len(connections.keys())))
 for k, v in connections.items():
     all_x = [c[0] for c in v]
     all_y = [c[1] for c in v]
     plt.scatter(all_x, all_y, linestyle='solid', color=colors[list(connections.keys()).index(k)])
     plt.plot(all_x, all_y, color=colors[list(connections.keys()).index(k)])
+# Injector connections
+all_x = [t[0] for t in INJ_relcoords.values()]
+all_y = [t[1] for t in INJ_relcoords.values()]
+ax.scatter(all_x, all_y)
+for i, txt in enumerate(INJ_relcoords.keys()):
+    ax.annotate(txt, (all_x[i] + 2, all_y[i] + 2))
 
 # color=colors[list(PRO_relcoords.keys()).index(k)]
 list(chain.from_iterable(discrete_links))
